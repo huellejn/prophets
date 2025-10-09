f9d34e1e6d7fa56e63d689d9fea14b3b2bda8b96
jhuellein <jennifer.huellein@nct-heidelberg.de>
2025-10-09 09:49:05 +0200

    update kernelKM function

--- R/kernelKM_PFSr.R
+++ R/kernelKM_PFSr.R
@@ -4,7 +4,7 @@
 #' @param ratio.name name of the column that contains the PFS1/PFS2 ratio ('numeric')
 #' @param status.name name of the column that contains the status ('boolean')
 #' @param time0.name name of the column that contains the PFS1 ('numeric')
-#' @param delta a numeric value indicating the desired difference between PFS2 and PFS1 that is seen as a success. 
+#' @param delta a numeric value indicating the desired difference between PFS2 and PFS1 that is seen as a success.
 #' @param conf.int a boolean indicating if confidende intervals should be calculated
 #' @param n.boot an integer value indicating the number of permutations to obtain confidence intervals for the survival estimates
 #'
@@ -16,68 +16,95 @@
 #' kernelKM_PFSr(data, "ratio", "status", "PFS1")
 #' # Example with modified values
 #' kernelKM_PFSr(data, "ratio", "status", "PFS1", delta = 2, conf.int = TRUE)
-kernelKM_PFSr <- function(data, ratio.name = "ratio", status.name = "status", time0.name = "PFS1", delta = NULL, conf.int = FALSE, n.boot = 2000) {
-  
-  result = estimator(data, ratio.name, status.name, time0.name)
-  ratio = result$ratio
-  surv = result$surv
-  
-  if( min(surv) > 0.5 ) {
-    medsurv = NA
-  } else {
-    medsurv = ratio[sum(surv>0.5)+1]
-  }
-  
-  res = list(ratio=ratio, surv=surv, med=medsurv)
-  
-  # If conf.int is TRUE, calculate confidence intervals
-  if (conf.int) {
-    
-    bootresult = boot(n.boot, data, ratio.name, status.name, time0.name, ratio)
-    se = bootresult$se
-    low = c(1, exp(-exp(log(-log(surv[-1]))-1.96*se[-1]/surv[-1]/log(surv[-1]))))
-    upp = c(1, exp(-exp(log(-log(surv[-1]))+1.96*se[-1]/surv[-1]/log(surv[-1]))))
-    low.med = bootresult$low.med
-    upp.med= bootresult$upp.med
-    
-    res = c(res, list(se=se, low=low, upp=upp, low.med=low.med, upp.med=upp.med))
-    
-  }
-  
-  # If delta is not null
-  if( !is.null(delta) ) {
-    
-    index = apply(outer(ratio, delta, "<="), 2, sum)
-    surv.points = surv[index]
-    ind = which(delta > max(data[[ratio.name]]))
-    
-    if( length(ind) > 0 ) {
-      
-      surv.points[ind] = NA
-      print(paste0("The GMI survival probability is not estimable at points greater than ", 
-                   max(data[[ratio.name]]), 
-                   ", the largest GMI value in the data"))
-      
+kernelKM_PFSr <- function(
+  data,
+  ratio.name,
+  status.name,
+  time0.name,
+  delta = NULL,
+  conf.int = FALSE,
+  n.boot = 2000
+) {
+  # Get base estimates
+  result <- estimator(data, ratio.name, status.name, time0.name)
+  ratio <- result$ratio
+  surv <- result$surv
+
+  # Calculate median survival
+  medsurv <- ifelse(min(surv) > 0.5, NA, ratio[sum(surv > 0.5) + 1])
+
+  # Helper function to get survival at delta points
+  get_surv_at_delta <- function(surv_vec, ratio, delta, data, ratio.name) {
+    index <- apply(outer(ratio, delta, "<="), 2, sum)
+    surv_points <- surv_vec[index]
+
+    # Handle values beyond data range
+    ind <- which(delta > max(data[[ratio.name]]))
+    if (length(ind) > 0) {
+      surv_points[ind] <- NA
+      message(
+        "PFSratio survival probability is not estimable at delta > ",
+        max(data[[ratio.name]]),
+        " (max PFSratio in data)"
+      )
     }
-    
-    res = c(res, list(delta=delta, surv.points=surv.points))
-    
-    if(conf.int) {
-      
-      se.points = se[index]
-      low.points = low[index]
-      upp.points = upp[index]
-      
-      if (length(ind)>0){
-        se.points[ind] = NA; low.points[ind] = NA; upp.points[ind] = NA
-      }
-      
-      res = c(res, list( se.points=se.points, low.points=low.points, upp.points=upp.points))
-      
-    }
-    
+
+    surv_points
   }
-  
-  return(res)
-  
+
+  # Without confidence intervals
+  if (!conf.int) {
+    if (!is.null(delta)) {
+      surv_points <- get_surv_at_delta(surv, ratio, delta, data, ratio.name)
+      return(list(
+        ratio = ratio,
+        surv = surv,
+        `median PFSratio` = medsurv,
+        delta = delta,
+        `PFSr_estimator` = surv_points
+      ))
+    }
+    return(list(ratio = ratio, surv = surv, `median PFSratio` = medsurv))
+  }
+
+  # With confidence intervals
+  bootresult <- boot(n.boot, data, ratio.name, status.name, time0.name, ratio)
+  se <- bootresult$se
+
+  # Calculate confidence bounds
+  log_transform <- log(-log(surv[-1]))
+  se_ratio <- se[-1] / (surv[-1] * log(surv[-1]))
+
+  low <- c(1, exp(-exp(log_transform - 1.96 * se_ratio)))
+  upp <- c(1, exp(-exp(log_transform + 1.96 * se_ratio)))
+
+  if (!is.null(delta)) {
+    # Get point estimates at delta
+    surv_points <- get_surv_at_delta(surv, ratio, delta, data, ratio.name)
+    index <- apply(outer(ratio, delta, "<="), 2, sum)
+
+    out <- tibble(
+      delta = delta,
+      estimate = surv_points,
+      conf.low = low[index],
+      conf.high = upp[index]
+    )
+
+    survtab <- tibble(
+      `median PFSratio` = medsurv,
+      conf.low = bootresult$low.med,
+      conf.high = bootresult$upp.med
+    )
+
+    return(list(`median PFSratio` = survtab, `PFSr_estimator` = out))
+  }
+
+  list(
+    ratio = ratio,
+    surv = surv,
+    se = se,
+    low = low,
+    upp = upp,
+    `median PFSratio` = medsurv
+  )
 }
